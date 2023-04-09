5dca14c58bfb37ce32f7cef3655e8d72bc160228
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-09 18:26:43 +0530

    add submission logic

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import axios from "axios";
+
 const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDeault();
+    await axios.post("http://localhost:8000/posts", {
+      title,
+    });
+    setTitle("");
+  };
+
   return (
     <div>
       <h2>Create Post</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
