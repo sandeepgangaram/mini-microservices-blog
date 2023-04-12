de6279e2543858cc3e51766b2fc85651dc4d154a
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-12 08:21:30 +0530

    add emit events endpoint on posts

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const { title } = req.body;
   const id = randomBytes(4).toString("hex");
 
@@ -22,6 +23,13 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  await axios.post("http://localhost:8005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
   res.status(201).send(posts[id]);
 });
 
