af426f4d130ab808225acaa440529224bc91b4f9
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-12 15:38:06 +0530

    add moderation logic

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,25 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    let status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:8005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+
+  res.send({ status: "OK" });
+});
 
 app.listen(8003, () => {
   console.log("Listening on port 8003");
