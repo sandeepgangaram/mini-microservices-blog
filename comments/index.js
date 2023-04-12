09503250a18601379ac4c3b3f4ee772be182d173
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-12 15:38:24 +0530

    modify data and psot events to bus

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   await axios.post("http://localhost:8005/events", {
     type: "CommentCreated",
@@ -32,6 +32,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId,
+      status: "pending",
     },
   });
 
@@ -40,8 +41,33 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received", req.body.type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("Event Received", type);
+
+  if (type === "CommentModerated") {
+    const { id, content, postId, status } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:8005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
+
+  res.send({ status: "OK" });
 });
 
 app.listen(8001, () => {
