3239a8f400ab96ecf09f84b1eb5bdc21c95eaef3
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-12 08:32:47 +0530

    emit commentCreated event

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -15,17 +16,26 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const { id } = req.params;
+app.post("/posts/:id/comments", async (req, res) => {
+  const { id: postId } = req.params;
   const { content } = req.body;
 
   const commentId = randomBytes(4).toString("hex");
 
-  const comments = commentsByPostId[id] || [];
+  const comments = commentsByPostId[postId] || [];
 
   comments.push({ commentId, content });
 
-  commentsByPostId[id] = comments;
+  await axios.post("http://localhost:8005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  });
+
+  commentsByPostId[postId] = comments;
 
   res.status(201).send(comments);
 });
