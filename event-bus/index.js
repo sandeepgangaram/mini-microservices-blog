433f903a8f66098b6f28eb40ea8a2ff5e4d73972
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-12 08:21:51 +0530

    fix endpoint error

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,10 +4,10 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-app.post("/posts", (req, res) => {
-  axios.post("http://localhost:8000/posts");
-  axios.post("http://localhost:8001/posts");
-  axios.post("http://localhost:8002/posts");
+app.post("/events", (req, res) => {
+  axios.post("http://localhost:8000/events");
+  axios.post("http://localhost:8001/events");
+  axios.post("http://localhost:8002/events");
 
   res.send({ status: "OK" });
 });
